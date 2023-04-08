6da231ba6dcaecb2cae19e0a2bba8f41030f84cf
Martha <martha.griggs9@gmail.com>
2023-04-07 21:34:18 -0500

    adding folders and files to repository

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,102 @@
+// Store our API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that describes the place and time of the earthquake.
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><hr><p>Magnitude: ${feature.properties.mag}</p>`);
+  }
+
+  //Create a GeoJSON layer containing the features array on the earthquakeData object
+  function CreatCircleMarker(feature, latlng) {
+    let options = {
+        radius: feature.properties.mag*5, 
+        fillColor: chooseColor(feature.properties.mag),
+        color: chooseColor(feature.properties.mag),
+        weight: 1,
+        opacity: 0.8,
+        fillOpacity: 0.35
+    }
+    return L.circleMarker(latlng, options);
+  }
+
+  // Create a variable for earthquakes to house latlng, each feature fo popup and circle 
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: CreatCircleMarker
+  });
+
+  // Send our earthquakes layer to the createMap function/
+  createMap(earthquakes);
+}
+
+
+//circle color palette based on mag (feature) data marker. 
+//Data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color
+//Earthquakes with higher magnitudes should appear larger 
+//Earthquakes with greater depth should appear darker in color
+function chooseColor(mag) {
+    switch(true) {
+        case(1.0 <= mag && mag <= 2.5):
+            return "#0071BC"; // Strong blue
+        case (2.5 <= mag && mag <=4.0):
+            return "#35BC00";
+        case (4.0 <= mag && mag <=5.5):
+            return "#BCBC00";
+        case (5.5 <= mag && mag <= 8.0):
+            return "#BC3500";
+        case (8.0 <= mag && mag <=20.0):
+            return "#BC0000";
+        default:
+            return "#E2FFAE";
+    }
+}
+
+
+function createMap(earthquakes) {
+
+  // Create the base layers.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  // Create a baseMaps object.
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
+
+  // Create an overlay object to hold our overlay.
+  let overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [street, earthquakes]
+  });
+
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+}
